409e0dad3db2c6f2f0703854157a31bf30838416
Tan-Barbudo <servicios.zamora86@gmail.com>
2026-06-09 02:36:08 -0300

    vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,8 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    plugins: [react(), tailwindcss()],
+  base: '/New-Curso-Globoflexia-Junio/',
+  plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
         '@': path.resolve(__dirname, '.'),
